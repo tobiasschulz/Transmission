2c6a776b05c5823dece0e86eab264c80095929df
livings124 <livings124@f4695dd4-2c0a-0410-b89c-da849a56a58e>
2006-12-16 05:17:25 +0000

    a little cleaner

--- branches/multitracker/macosx/Torrent.m
+++ branches/multitracker/macosx/Torrent.m
@@ -355,12 +355,8 @@ static uint32_t kRed   = BE(0xFF6450FF), //255, 100, 80
     BOOL wasError = fError;
     if ((fError = fStat->cannotConnect))
     {
-        if (!wasError && [self isActive])
-        {
-            fError = YES;
-            if (![self isSeeding])
-                [[NSNotificationCenter defaultCenter] postNotificationName: @"StoppedDownloading" object: self];
-        }
+        if (!wasError && [self isActive] && ![self isSeeding])
+            [[NSNotificationCenter defaultCenter] postNotificationName: @"StoppedDownloading" object: self];
     }
 
     if ([self isActive] && fStat->status != TR_STATUS_CHECK )
