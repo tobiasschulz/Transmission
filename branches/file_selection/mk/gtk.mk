d8372d80cbb3d5b95a24725e628859de5808f60e
charles <charles@f4695dd4-2c0a-0410-b89c-da849a56a58e>
2007-06-13 04:25:41 +0000

    fix r2062 oops

--- branches/file_selection/mk/gtk.mk
+++ branches/file_selection/mk/gtk.mk
@@ -8,7 +8,7 @@ SRCS = actions.c conf.c dialogs.c hig.c io.c ipc.c main.c msgwin.c \
        tr_icon.c tr_prefs.c tr_torrent.c tr_window.c util.c
 OBJS = $(SRCS:%.c=%.o)
 
-CFLAGS  += $(CFLAGS_GTK)
+CFLAGS  += $(CFLAGS_GTK) -I../libtransmission
 LDFLAGS += $(LDFLAGS_GTK)
 LDLIBS  += ../libtransmission/libtransmission.a
 
