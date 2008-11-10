eb006128f48453d7c7e49b5434581ce9a6513a47
charles <charles@f4695dd4-2c0a-0410-b89c-da849a56a58e>
2008-11-10 14:35:11 +0000

    branching for 1.4x

---
+++ branches/1.4x/libtransmission/net.h
@@ -0,0 +1,92 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Copyright (c) 2005-2008 Transmission authors and contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *****************************************************************************/
+
+#ifndef _TR_NET_H_
+#define _TR_NET_H_
+
+#ifdef WIN32
+ #include <inttypes.h>
+ #include <winsock2.h>
+ typedef int socklen_t;
+ typedef uint16_t tr_port_t;
+#elif defined( __BEOS__ )
+ #include <sys/socket.h>
+ #include <netinet/in.h>
+ typedef unsigned short tr_port_t;
+ typedef int socklen_t;
+#else
+ #include <sys/types.h>
+ #include <sys/socket.h>
+ #include <netinet/in.h>
+ #include <arpa/inet.h>
+ typedef in_port_t tr_port_t;
+#endif
+
+#ifdef WIN32
+ #define ECONNREFUSED WSAECONNREFUSED
+ #define ECONNRESET   WSAECONNRESET
+ #define EHOSTUNREACH WSAEHOSTUNREACH
+ #define EINPROGRESS  WSAEINPROGRESS
+ #define ENOTCONN     WSAENOTCONN
+ #define EWOULDBLOCK  WSAEWOULDBLOCK
+ #define sockerrno WSAGetLastError( )
+#else
+ #include <errno.h>
+ #define sockerrno errno
+#endif
+
+struct in_addr;
+struct sockaddr_in;
+
+/***********************************************************************
+ * DNS resolution
+ **********************************************************************/
+int  tr_netResolve( const  char *,
+                    struct in_addr * );
+
+
+/***********************************************************************
+ * Sockets
+ **********************************************************************/
+int  tr_netOpenTCP( const struct in_addr * addr,
+                    tr_port_t              port );
+
+int  tr_netBindTCP( int port );
+
+int  tr_netAccept( int    s,
+                   struct in_addr *,
+                          tr_port_t * );
+
+int  tr_netSetTOS( int s,
+                   int tos );
+
+void tr_netClose( int s );
+
+void tr_netNtop( const struct in_addr * addr,
+                 char *                 buf,
+                 int                    len );
+
+void tr_netInit( void );
+
+#endif /* _TR_NET_H_ */
---
+++ branches/1.4x/macosx/version.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+#
+# $Id$
+
+# convention: -TR MAJOR MINOR MAINT STATUS - (each a single char)
+# STATUS: "X" for prerelease test builds,
+#         "Z" for unsupported trunk builds,
+#         "0" for stable, supported releases
+# these should be the only two lines you need to change
+PEERID_PREFIX="-TR140Z-"
+USERAGENT_PREFIX="1.40+"
+
+SVN_REVISION=`find ./macosx ./libtransmission -name "*\.[chmp]" -o -name "*\.cpp" -o -name "*\.po" -o -name "*\.sh" | \
+              xargs grep "\$Id:" | \
+              grep -v third-party | \
+              cut -d"$Id:" -f3 | cut -d" " -f3 | sort -n | tail -n 1`
+  
+# Generate files to be included: only overwrite them if changed so make
+# won't rebuild everything unless necessary
+replace_if_differs ()
+{
+    if cmp $1 $2 > /dev/null 2>&1; then
+      rm -f $1
+    else
+      mv -f $1 $2
+    fi
+}
+
+# Generate version.h
+cat > libtransmission/version.h.new << EOF
+#define PEERID_PREFIX             "$PEERID_PREFIX"
+#define USERAGENT_PREFIX          "$USERAGENT_PREFIX"
+#define SVN_REVISION              "$SVN_REVISION"
+#define SHORT_VERSION_STRING      "$USERAGENT_PREFIX"
+#define LONG_VERSION_STRING       "$USERAGENT_PREFIX ($SVN_REVISION)"
+
+#define VERSION_STRING_INFOPLIST  $USERAGENT_PREFIX
+#define BUNDLE_VERSION_INFOPLIST  $SVN_REVISION
+EOF
+replace_if_differs libtransmission/version.h.new libtransmission/version.h
+
+exit 0
