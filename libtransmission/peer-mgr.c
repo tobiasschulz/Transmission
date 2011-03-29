tobiasschulz/Transmission

commit 84fd461d5013f856298a40a04cb491ca70afde1b

(trunk libT) break the mac build and introduce new crashes.
This is partially to address #4145 "Downloads stuck at 100%" by refactoring the bitset, bitfield, and tr_completion; however, the ripple effect is larger than usual so things may get worse in the short term before getting better.

livings124: to fix the mac build, remove bitset.[ch] from xcode


git-svn-id: svn://svn.transmissionbt.com/Transmission/trunk@12248 f4695dd4-2c0a-0410-b89c-da849a56a58e

libtransmission/peer-mgr.c
@@ -374,7 +374,7 @@ tr_peerConstruct( tr_peer * peer )
 {
     memset( peer, 0, sizeof( tr_peer ) );
 
-    peer->have = TR_BITSET_INIT;
+    peer->have = TR_BITFIELD_INIT;
 
     tr_historyConstruct( &peer->blocksSentToClient,  CANCEL_HISTORY_SEC, ( RECHOKE_PERIOD_MSEC / 1000 ) );
     tr_historyConstruct( &peer->blocksSentToPeer,    CANCEL_HISTORY_SEC, ( RECHOKE_PERIOD_MSEC / 1000 ) );
@@ -406,6 +406,8 @@ getPeer( Torrent * torrent, struct peer_atom * atom )
     if( peer == NULL )
     {
         peer = peerNew( atom );
+        tr_bitfieldConstruct( &peer->have, torrent->tor->blockCount );
+        tr_bitfieldConstruct( &peer->blame, torrent->tor->blockCount );
         tr_ptrArrayInsertSorted( &torrent->peers, peer, peerCompare );
     }
 
@@ -419,12 +421,14 @@ tr_peerDestruct( tr_torrent * tor, tr_peer * peer )
 {
     assert( peer != NULL );
 
-    peerDeclinedAllRequests( tor->torrentPeers, peer );
+    if( tor->torrentPeers->isRunning )
+        peerDeclinedAllRequests( tor->torrentPeers, peer );
 
     if( peer->msgs != NULL )
         tr_peerMsgsFree( peer->msgs );
 
     if( peer->io ) {
+fprintf( stderr, "[%p] peer %p clearing/unreffing its io (%s:%d)\n", peer->io, peer, __FILE__, __LINE__ );
         tr_peerIoClear( peer->io );
         tr_peerIoUnref( peer->io ); /* balanced by the ref in handshakeDoneCB() */
     }
@@ -434,8 +438,8 @@ tr_peerDestruct( tr_torrent * tor, tr_peer * peer )
     tr_historyDestruct( &peer->cancelsSentToClient );
     tr_historyDestruct( &peer->cancelsSentToPeer   );
 
-    tr_bitsetDestruct( &peer->have );
-    tr_bitfieldFree( peer->blame );
+    tr_bitfieldDestruct( &peer->have );
+    tr_bitfieldDestruct( &peer->blame );
     tr_free( peer->client );
 
     if( peer->atom )
@@ -482,7 +486,7 @@ replicationNew( Torrent * t )
         uint16_t r = 0;
 
         for( peer_i=0; peer_i<peer_count; ++peer_i )
-            if( tr_bitsetHas( &peers[peer_i]->have, piece_i ) )
+            if( tr_bitfieldHas( &peers[peer_i]->have, piece_i ) )
                 ++r;
 
         t->pieceReplication[piece_i] = r;
@@ -866,15 +870,21 @@ countActiveWebseeds( const Torrent * t )
     return activeCount;
 }
 
+static bool
+testForEndgame( const Torrent * t )
+{
+    /* we consider ourselves to be in endgame if the number of bytes
+       we've got requested is >= the number of bytes left to download */
+    return ( t->requestCount * t->tor->blockSize )
+               <= tr_cpLeftUntilDone( &t->tor->completion );
+}
+
 static void
 updateEndgame( Torrent * t )
 {
-    const tr_torrent * tor = t->tor;
-    const tr_block_index_t missing = tr_cpBlocksMissing( &tor->completion );
-
     assert( t->requestCount >= 0 );
 
-    if( (tr_block_index_t) t->requestCount < missing )
+    if( !testForEndgame( t ) )
     {
         /* not in endgame */
         t->endgame = 0;
@@ -1267,7 +1277,7 @@ tr_incrReplication( Torrent * t )
  * Decrease the replication count of pieces present in the bitset.
  */
 static void
-tr_decrReplicationFromBitset( Torrent * t, const tr_bitset * bitset )
+tr_decrReplicationFromBitfield( Torrent * t, const tr_bitfield * b )
 {
     int i;
     const int n = t->pieceReplicationSize;
@@ -1275,15 +1285,13 @@ tr_decrReplicationFromBitset( Torrent * t, const tr_bitset * bitset )
     assert( replicationExists( t ) );
     assert( t->pieceReplicationSize == t->tor->info.pieceCount );
 
-    if( bitset->haveAll )
+    if( tr_bitfieldHasAll( b ) )
     {
         for( i=0; i<n; ++i )
             --t->pieceReplication[i];
     }
-    else if ( !bitset->haveNone )
+    else if ( !tr_bitfieldHasNone( b ) )
     {
-        const tr_bitfield * const b = &bitset->bitfield;
-
         for( i=0; i<n; ++i )
             if( tr_bitfieldHas( b, i ) )
                 --t->pieceReplication[i];
@@ -1316,7 +1324,7 @@ tr_peerMgrGetNextRequests( tr_torrent           * tor,
     int got;
     Torrent * t;
     struct weighted_piece * pieces;
-    const tr_bitset * have = &peer->have;
+    const tr_bitfield * const have = &peer->have;
 
     /* sanity clause */
     assert( tr_isTorrent( tor ) );
@@ -1345,7 +1353,7 @@ tr_peerMgrGetNextRequests( tr_torrent           * tor,
         struct weighted_piece * p = pieces + i;
 
         /* if the peer has this piece that we want... */
-        if( tr_bitsetHas( have, p->index ) )
+        if( tr_bitfieldHas( have, p->index ) )
         {
             tr_block_index_t b;
             tr_block_index_t first;
@@ -2212,7 +2220,7 @@ tr_peerMgrSetBlame( tr_torrent     * tor,
         for( i = 0; i < peerCount; ++i )
         {
             tr_peer * peer = peers[i];
-            if( tr_bitfieldHas( peer->blame, pieceIndex ) )
+            if( tr_bitfieldHas( &peer->blame, pieceIndex ) )
             {
                 tordbg( t, "peer %s contributed to corrupt piece (%d); now has %d strikes",
                         tr_atomAddrStr( peer->atom ),
@@ -2402,7 +2410,7 @@ tr_peerMgrStartTorrent( tr_torrent * tor )
 static void
 stopTorrent( Torrent * t )
 {
-    int i, n;
+    tr_peer * peer;
 
     t->isRunning = false;
 
@@ -2410,9 +2418,8 @@ stopTorrent( Torrent * t )
     invalidatePieceSorting( t );
 
     /* disconnect the peers. */
-    for( i=0, n=tr_ptrArraySize( &t->peers ); i<n; ++i )
-        peerDelete( t, tr_ptrArrayNth( &t->peers, i ) );
-    tr_ptrArrayClear( &t->peers );
+    while(( peer = tr_ptrArrayPop( &t->peers )))
+        peerDelete( t, peer );
 
     /* disconnect the handshakes. handshakeAbort calls handshakeDoneCB(),
      * which removes the handshake from t->outgoingHandshakes... */
@@ -2452,24 +2459,24 @@ tr_peerMgrRemoveTorrent( tr_torrent * tor )
 void
 tr_peerUpdateProgress( tr_torrent * tor, tr_peer * peer )
 {
-    const tr_bitset * have = &peer->have;
+    const tr_bitfield * have = &peer->have;
 
-    if( have->haveAll )
+    if( tr_bitfieldHasAll( have ) )
     {
         peer->progress = 1.0;
     }
-    else if( have->haveNone )
+    else if( tr_bitfieldHasNone( have ) )
     {
         peer->progress = 0.0;
     }
     else
     {
-        const float trueCount = tr_bitfieldCountTrueBits( &have->bitfield );
+        const float true_count = tr_bitfieldCountTrueBits( have );
 
         if( tr_torrentHasMetadata( tor ) )
-            peer->progress = trueCount / tor->info.pieceCount;
+            peer->progress = true_count / tor->info.pieceCount;
         else /* without pieceCount, this result is only a best guess... */
-            peer->progress = trueCount / ( have->bitfield.bitCount + 1 );
+            peer->progress = true_count / ( have->bit_count + 1 );
     }
 
     if( peer->atom && ( peer->progress >= 1.0 ) )
@@ -2516,35 +2523,76 @@ tr_peerMgrTorrentAvailability( const tr_torrent * tor, int8_t * tab, unsigned in
             else if( peerCount ) {
                 int j;
                 for( j=0; j<peerCount; ++j )
-                    if( tr_bitsetHas( &peers[j]->have, piece ) )
+                    if( tr_bitfieldHas( &peers[j]->have, piece ) )
                         ++tab[i];
             }
         }
     }
 }
 
-/* Returns the pieces that are available from peers */
-tr_bitfield*
-tr_peerMgrGetAvailable( const tr_torrent * tor )
+static bool
+peerIsSeed( const tr_peer * peer )
 {
-    int i;
-    Torrent * t = tor->torrentPeers;
-    const int peerCount = tr_ptrArraySize( &t->peers );
-    const tr_peer ** peers = (const tr_peer**) tr_ptrArrayBase( &t->peers );
-    tr_bitfield * pieces = tr_bitfieldNew( t->tor->info.pieceCount );
+    if( peer->progress >= 1.0 )
+        return true;
 
-    assert( tr_torrentIsLocked( tor ) );
+    if( peer->atom && atomIsSeed( peer->atom ) )
+        return true;
 
-    for( i=0; i<peerCount; ++i )
-        tr_bitsetOr( pieces, &peers[i]->have );
+    return false;
+}
 
-    return pieces;
+/* count how many pieces we want that connected peers have */
+uint64_t
+tr_peerMgrGetDesiredAvailable( const tr_torrent * tor )
+{
+    size_t i;
+    size_t n;
+    uint64_t desiredAvailable;
+    const Torrent * t = tor->torrentPeers;
+
+    /* common shortcuts... */
+
+    if( tr_torrentIsSeed( t->tor ) )
+        return 0;
+
+    if( !tr_torrentHasMetadata( tor ) )
+        return 0;
+
+    n = tr_ptrArraySize( &t->peers );
+    if( n == 0 )
+        return 0;
+    else {
+        const tr_peer ** peers = (const tr_peer**) tr_ptrArrayBase( &t->peers );
+        for( i=0; i<n; ++i )
+            if( peers[i]->atom && atomIsSeed( peers[i]->atom ) )
+                return tr_cpLeftUntilDone( &tor->completion );
+    }
+
+    if( !t->pieceReplication || !t->pieceReplicationSize )
+        return 0;
+
+    /* do it the hard way */
+
+    desiredAvailable = 0;
+    for( i=0, n=tor->info.pieceCount; i<n; ++i )
+    {
+        if( tor->info.pieces[i].dnd )
+            continue;
+        if( t->pieceReplicationSize >= i )
+            continue;
+        if( t->pieceReplication[i] == 0 )
+            continue;
+
+        desiredAvailable += tr_cpMissingBytesInPiece( &t->tor->completion, i );
+    }
+
+    return desiredAvailable;
 }
 
 void
 tr_peerMgrTorrentStats( tr_torrent  * tor,
                         int         * setmePeersConnected,
-                        int         * setmeSeedsConnected,
                         int         * setmeWebseedsSendingToUs,
                         int         * setmePeersSendingToUs,
                         int         * setmePeersGettingFromUs,
@@ -2560,7 +2608,6 @@ tr_peerMgrTorrentStats( tr_torrent  * tor,
     size = tr_ptrArraySize( &t->peers );
 
     *setmePeersConnected       = 0;
-    *setmeSeedsConnected       = 0;
     *setmePeersGettingFromUs   = 0;
     *setmePeersSendingToUs     = 0;
     *setmeWebseedsSendingToUs  = 0;
@@ -2585,9 +2632,6 @@ tr_peerMgrTorrentStats( tr_torrent  * tor,
 
         if( clientIsUploadingTo( peer ) )
             ++*setmePeersGettingFromUs;
-
-        if( atomIsSeed( atom ) )
-            ++*setmeSeedsConnected;
     }
 
     *setmeWebseedsSendingToUs = countActiveWebseeds( t );
@@ -2623,18 +2667,6 @@ int
 tr_peerGetPieceSpeed_Bps( const tr_peer * peer, uint64_t now, tr_direction direction )
 {
     return peer->io ? tr_peerIoGetPieceSpeed_Bps( peer->io, now, direction ) : 0.0;
-}
-
-static bool
-peerIsSeed( const tr_peer * peer )
-{
-    if( peer->progress >= 1.0 )
-        return true;
-
-    if( peer->atom && atomIsSeed( peer->atom ) )
-        return true;
-
-    return false;
 }
 
 struct tr_peer_stat *
@@ -2734,7 +2766,7 @@ isPieceInteresting( const tr_torrent * tor, const tr_peer * peer, tr_piece_index
 {
     return ( !tor->info.pieces[index].dnd ) /* we want it */
         && ( !tr_cpPieceIsComplete( &tor->completion, index ) )  /* we don't have it */
-        && ( tr_bitsetHas( &peer->have, index ) ); /* peer has it */
+        && ( tr_bitfieldHas( &peer->have, index ) ); /* peer has it */
 }
 
 /* does this peer have any pieces that we want? */
@@ -3233,7 +3265,7 @@ removePeer( Torrent * t, tr_peer * peer )
     removed = tr_ptrArrayRemoveSorted( &t->peers, peer, peerCompare );
 
     if( replicationExists( t ) )
-        tr_decrReplicationFromBitset( t, &peer->have );
+        tr_decrReplicationFromBitfield( t, &peer->have );
 
     assert( removed == peer );
     peerDelete( t, removed );

libtransmission/Makefile.am
@@ -23,7 +23,6 @@ libtransmission_a_SOURCES = \
     bandwidth.c \
     bencode.c \
     bitfield.c \
-    bitset.c \
     blocklist.c \
     cache.c \
     clients.c \
@@ -75,7 +74,6 @@ noinst_HEADERS = \
     bandwidth.h \
     bencode.h \
     bitfield.h \
-    bitset.h \
     blocklist.h \
     cache.h \
     clients.h \

libtransmission/bitfield.c
@@ -15,157 +15,15 @@
 
 #include "transmission.h"
 #include "bitfield.h"
-#include "bitset.h"
 #include "utils.h" /* tr_new0() */
 
-const tr_bitfield TR_BITFIELD_INIT = { NULL, 0, 0 };
+const tr_bitfield TR_BITFIELD_INIT = { NULL, 0, 0, 0, false, false };
 
-tr_bitfield*
-tr_bitfieldConstruct( tr_bitfield * b, size_t bitCount )
-{
-    b->bitCount = bitCount;
-    b->byteCount = ( bitCount + 7u ) / 8u;
-    b->bits = tr_new0( uint8_t, b->byteCount );
-    return b;
-}
+/****
+*****
+****/
 
-tr_bitfield*
-tr_bitfieldDestruct( tr_bitfield * b )
-{
-    if( b )
-        tr_free( b->bits );
-    return b;
-}
-
-tr_bitfield*
-tr_bitfieldNew( size_t bitCount )
-{
-    return tr_bitfieldConstruct( tr_new( tr_bitfield, 1 ), bitCount );
-}
-
-void
-tr_bitfieldFree( tr_bitfield * b )
-{
-    tr_free( tr_bitfieldDestruct( b ) );
-}
-
-void
-tr_bitfieldClear( tr_bitfield * bitfield )
-{
-    memset( bitfield->bits, 0, bitfield->byteCount );
-}
-
-int
-tr_bitfieldAdd( tr_bitfield * bitfield,
-                size_t        nth )
-{
-    assert( bitfield );
-    assert( bitfield->bits );
-
-    if( nth >= bitfield->bitCount )
-        return -1;
-
-    bitfield->bits[nth >> 3u] |= ( 0x80 >> ( nth & 7u ) );
-    return 0;
-}
-
-/* Sets bit range [begin, end) to 1 */
-int
-tr_bitfieldAddRange( tr_bitfield * b,
-                     size_t        begin,
-                     size_t        end )
-{
-    size_t        sb, eb;
-    unsigned char sm, em;
-
-    end--;
-
-    if( ( end >= b->bitCount ) || ( begin > end ) )
-        return -1;
-
-    sb = begin >> 3;
-    sm = ~( 0xff << ( 8 - ( begin & 7 ) ) );
-    eb = end >> 3;
-    em = 0xff << ( 7 - ( end & 7 ) );
-
-    if( sb == eb )
-    {
-        b->bits[sb] |= ( sm & em );
-    }
-    else
-    {
-        b->bits[sb] |= sm;
-        b->bits[eb] |= em;
-        if( ++sb < eb )
-            memset ( b->bits + sb, 0xff, eb - sb );
-    }
-
-    return 0;
-}
-
-int
-tr_bitfieldRem( tr_bitfield * bitfield,
-                size_t        nth )
-{
-    assert( bitfield );
-    assert( bitfield->bits );
-
-    if( nth >= bitfield->bitCount )
-        return -1;
-
-    bitfield->bits[nth >> 3u] &= ( 0xff7f >> ( nth & 7u ) );
-    return 0;
-}
-
-/* Clears bit range [begin, end) to 0 */
-int
-tr_bitfieldRemRange( tr_bitfield * b,
-                     size_t        begin,
-                     size_t        end )
-{
-    size_t        sb, eb;
-    unsigned char sm, em;
-
-    end--;
-
-    if( ( end >= b->bitCount ) || ( begin > end ) )
-        return -1;
-
-    sb = begin >> 3;
-    sm = 0xff << ( 8 - ( begin & 7 ) );
-    eb = end >> 3;
-    em = ~( 0xff << ( 7 - ( end & 7 ) ) );
-
-    if( sb == eb )
-    {
-        b->bits[sb] &= ( sm | em );
-    }
-    else
-    {
-        b->bits[sb] &= sm;
-        b->bits[eb] &= em;
-        if( ++sb < eb )
-            memset ( b->bits + sb, 0, eb - sb );
-    }
-
-    return 0;
-}
-
-tr_bitfield*
-tr_bitfieldOr( tr_bitfield * a, const tr_bitfield * b )
-{
-    uint8_t * ait = a->bits;
-    const uint8_t * aend = ait + a->byteCount;
-    const uint8_t * bit = b->bits;
-    const uint8_t * bend = bit + b->byteCount;
-
-    while( ait!=aend && bit!=bend )
-        *ait++ |= *bit++;
-
-    return a;
-}
-
-static const int trueBitCount[256] =
+static const int8_t trueBitCount[256] =
 {
     0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
     1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
@@ -185,29 +43,14 @@ static const int trueBitCount[256] =
     4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8
 };
 
-size_t
-tr_bitfieldCountTrueBits( const tr_bitfield* b )
-{
-    size_t           ret = 0;
-    const uint8_t *  it, *end;
-
-    if( !b )
-        return 0;
-
-    for( it = b->bits, end = it + b->byteCount; it != end; ++it )
-        ret += trueBitCount[*it];
-
-    return ret;
-}
-
-size_t
-tr_bitfieldCountRange( const tr_bitfield * b, size_t begin, size_t end )
+static size_t
+countRange( const tr_bitfield * b, size_t begin, size_t end )
 {
     size_t ret = 0;
     const int first_byte = begin >> 3u;
     const int last_byte = ( end - 1 ) >> 3u;
 
-    assert( begin < end );
+    assert( b->bits != NULL );
 
     if( first_byte == last_byte )
     {
@@ -249,4 +92,274 @@ tr_bitfieldCountRange( const tr_bitfield * b, size_t begin, size_t end )
 
     assert( ret <= ( begin - end ) );
     return ret;
+}
+
+size_t
+tr_bitfieldCountRange( const tr_bitfield * b, size_t begin, size_t end )
+{
+    assert( begin < end );
+
+    if( tr_bitfieldHasAll( b ) )
+        return end - begin;
+
+    if( tr_bitfieldHasNone( b ) )
+        return 0;
+
+    return countRange( b, begin, end );
+}
+
+/***
+****
+***/
+
+static bool
+tr_bitfieldIsValid( const tr_bitfield * b )
+{
+    return ( b != NULL )
+        && ( b->bits || ( tr_bitfieldHasAll( b ) || tr_bitfieldHasNone( b )))
+        && ( b->byte_count <= b->bit_count )
+        && ( b->true_count <= b->bit_count )
+        && ( !b->bits || ( b->true_count == countRange( b, 0, b->bit_count )));
+}
+
+void*
+tr_bitfieldGetRaw( const tr_bitfield * b, size_t * byte_count )
+{
+    uint8_t * bits;
+
+    if( b->bits )
+    {
+        bits = tr_memdup( b->bits, b->byte_count );
+    }
+    else
+    {
+        assert( tr_bitfieldHasAll( b ) || tr_bitfieldHasNone( b ) );
+
+        bits = tr_new0( uint8_t, b->byte_count );
+
+        if( tr_bitfieldHasAll( b ) )
+        {
+            uint8_t val = 0xFF;
+            const int n = b->byte_count - 1;
+            memset( bits, val, n );
+            bits[n] = val << (b->byte_count*8 - b->bit_count);
+        }
+    }
+
+    *byte_count = b->byte_count;
+    return bits;
+}
+
+static void
+tr_bitfieldEnsureBitsAlloced( tr_bitfield * b )
+{
+    if( b->bits == NULL )
+        b->bits = tr_bitfieldGetRaw( b, &b->byte_count );
+
+    assert( tr_bitfieldIsValid( b ) );
+}
+
+static void
+tr_bitfieldSetTrueCount( tr_bitfield * b, size_t n )
+{
+    b->true_count = n;
+
+    if( tr_bitfieldHasAll( b ) || tr_bitfieldHasNone( b ) )
+    {
+        tr_free( b->bits );
+        b->bits = NULL;
+    }
+
+    assert( tr_bitfieldIsValid(  b ) );
+}
+
+static void
+tr_bitfieldRebuildTrueCount( tr_bitfield * b )
+{
+    tr_bitfieldSetTrueCount( b, countRange( b, 0, b->bit_count ) );
+}
+
+static void
+tr_bitfieldIncTrueCount( tr_bitfield * b, int i )
+{
+    tr_bitfieldSetTrueCount( b, b->true_count + i );
+}
+
+/****
+*****
+****/
+
+void
+tr_bitfieldConstruct( tr_bitfield * b, size_t bit_count )
+{
+    b->bit_count = bit_count;
+    b->byte_count = ( bit_count + 7u ) / 8u;
+    b->true_count = 0;
+    b->bits = NULL;
+    b->have_all_hint = false;
+    b->have_none_hint = false;
+
+    assert( tr_bitfieldIsValid( b ) );
+}
+
+void
+tr_bitfieldDestruct( tr_bitfield * b )
+{
+    tr_bitfieldSetHasNone( b );
+}
+
+static void
+tr_bitfieldClear( tr_bitfield * b )
+{
+    tr_free( b->bits );
+    b->bits = NULL;
+    b->true_count = 0;
+
+    assert( tr_bitfieldIsValid( b ) );
+}
+
+void
+tr_bitfieldSetHasNone( tr_bitfield * b )
+{
+    tr_bitfieldClear( b );
+    b->have_all_hint = false;
+    b->have_none_hint = true;
+}
+
+void
+tr_bitfieldSetHasAll( tr_bitfield * b )
+{
+    tr_bitfieldClear( b );
+    b->true_count = b->bit_count;
+    b->have_all_hint = true;
+    b->have_none_hint = false;
+}
+
+bool
+tr_bitfieldSetFromBitfield( tr_bitfield * b, const tr_bitfield * src )
+{
+    bool success = true;
+
+    if( tr_bitfieldHasAll( src ) )
+        tr_bitfieldSetHasAll( b );
+    else if( tr_bitfieldHasNone( src ) )
+        tr_bitfieldSetHasNone( b );
+    else
+        success = tr_bitfieldSetRaw( b, src->bits, src->byte_count );
+
+    return success;
+}
+
+bool
+tr_bitfieldSetRaw( tr_bitfield * b, const void * bits, size_t byte_count )
+{
+    const bool success = b->byte_count == byte_count;
+
+    if( success )
+    {
+        tr_bitfieldSetHasNone( b );
+        b->bits = tr_memdup( bits, byte_count );
+        tr_bitfieldRebuildTrueCount( b );
+    }
+
+    return success;
+}
+
+void
+tr_bitfieldAdd( tr_bitfield * b, size_t nth )
+{
+    assert( nth < b->bit_count );
+
+    if( !tr_bitfieldHas( b, nth ) )
+    {
+        tr_bitfieldEnsureBitsAlloced( b );
+        b->bits[nth >> 3u] |= ( 0x80 >> ( nth & 7u ) );
+        tr_bitfieldIncTrueCount( b, 1 );
+    }
+}
+
+/* Sets bit range [begin, end) to 1 */
+void
+tr_bitfieldAddRange( tr_bitfield * b, size_t begin, size_t end )
+{
+    size_t sb, eb;
+    unsigned char sm, em;
+    const size_t diff = (end-begin) - tr_bitfieldCountRange( b, begin, end );
+
+    if( diff == 0 )
+        return;
+
+    end--;
+    if( ( end >= b->bit_count ) || ( begin > end ) )
+        return;
+
+    sb = begin >> 3;
+    sm = ~( 0xff << ( 8 - ( begin & 7 ) ) );
+    eb = end >> 3;
+    em = 0xff << ( 7 - ( end & 7 ) );
+
+    tr_bitfieldEnsureBitsAlloced( b );
+    if( sb == eb )
+    {
+        b->bits[sb] |= ( sm & em );
+    }
+    else
+    {
+        b->bits[sb] |= sm;
+        b->bits[eb] |= em;
+        if( ++sb < eb )
+            memset ( b->bits + sb, 0xff, eb - sb );
+    }
+
+    tr_bitfieldIncTrueCount( b, diff );
+}
+
+void
+tr_bitfieldRem( tr_bitfield * b, size_t nth )
+{
+    assert( tr_bitfieldIsValid( b ) );
+
+    if( !tr_bitfieldHas( b, nth ) )
+    {
+        tr_bitfieldEnsureBitsAlloced( b );
+        b->bits[nth >> 3u] &= ( 0xff7f >> ( nth & 7u ) );
+        tr_bitfieldIncTrueCount( b, -1 );
+    }
+}
+
+/* Clears bit range [begin, end) to 0 */
+void
+tr_bitfieldRemRange( tr_bitfield * b, size_t begin, size_t end )
+{
+    size_t sb, eb;
+    unsigned char sm, em;
+    const size_t diff = tr_bitfieldCountRange( b, begin, end );
+
+    if( !diff )
+        return;
+
+    end--;
+
+    if( ( end >= b->bit_count ) || ( begin > end ) )
+        return;
+
+    sb = begin >> 3;
+    sm = 0xff << ( 8 - ( begin & 7 ) );
+    eb = end >> 3;
+    em = ~( 0xff << ( 7 - ( end & 7 ) ) );
+
+    tr_bitfieldEnsureBitsAlloced( b );
+    if( sb == eb )
+    {
+        b->bits[sb] &= ( sm | em );
+    }
+    else
+    {
+        b->bits[sb] &= sm;
+        b->bits[eb] &= em;
+        if( ++sb < eb )
+            memset ( b->bits + sb, 0, eb - sb );
+    }
+
+    tr_bitfieldIncTrueCount( b, -diff );
 }

libtransmission/bitfield.h
@@ -17,65 +17,111 @@
 #ifndef TR_BITFIELD_H
 #define TR_BITFIELD_H 1
 
+#include <assert.h>
 #include "transmission.h"
 
 /** @brief Implementation of the BitTorrent spec's Bitfield array of bits */
 typedef struct tr_bitfield
 {
     uint8_t *  bits;
-    size_t     bitCount;
-    size_t     byteCount;
+    size_t     bit_count;
+    size_t     byte_count;
+    size_t     true_count;
+
+    /* Special cases for when full or empty but we don't know the bitCount.
+       This occurs when a magnet link's peers send have all / have none */
+    bool       have_all_hint;
+    bool       have_none_hint;
 }
 tr_bitfield;
 
+/***
+****  life cycle
+***/
+
 extern const tr_bitfield TR_BITFIELD_INIT;
 
-tr_bitfield* tr_bitfieldConstruct( tr_bitfield*, size_t bitCount );
+void   tr_bitfieldConstruct( tr_bitfield*, size_t bit_count );
 
-tr_bitfield* tr_bitfieldDestruct( tr_bitfield* );
+void   tr_bitfieldDestruct( tr_bitfield* );
 
-tr_bitfield* tr_bitfieldNew( size_t bitCount );
+/***
+****
+***/
 
-void tr_bitfieldFree( tr_bitfield * b );
+void   tr_bitfieldSetHasAll( tr_bitfield* );
 
-void         tr_bitfieldClear( tr_bitfield* );
+void   tr_bitfieldSetHasNone( tr_bitfield* );
 
-int          tr_bitfieldAdd( tr_bitfield*, size_t bit );
+void   tr_bitfieldAdd( tr_bitfield*, size_t bit );
 
-int          tr_bitfieldRem( tr_bitfield*, size_t bit );
+void   tr_bitfieldRem( tr_bitfield*, size_t bit );
 
-int          tr_bitfieldAddRange( tr_bitfield *, size_t begin, size_t end );
+void   tr_bitfieldAddRange( tr_bitfield*, size_t begin, size_t end );
 
-int          tr_bitfieldRemRange( tr_bitfield*, size_t begin, size_t end );
+void   tr_bitfieldRemRange( tr_bitfield*, size_t begin, size_t end );
 
-size_t       tr_bitfieldCountTrueBits( const tr_bitfield* );
+/***
+****
+***/
 
-size_t       tr_bitfieldCountRange( const tr_bitfield * b, size_t begin, size_t end );
+bool   tr_bitfieldSetFromBitfield( tr_bitfield*, const tr_bitfield* );
 
+bool   tr_bitfieldSetRaw( tr_bitfield*, const void * bits, size_t byte_count );
 
-tr_bitfield* tr_bitfieldOr( tr_bitfield*, const tr_bitfield* );
+void*  tr_bitfieldGetRaw( const tr_bitfield * b, size_t * byte_count );
+
+/***
+****
+***/
+
+size_t  tr_bitfieldCountRange( const tr_bitfield*, size_t begin, size_t end );
+
+static inline size_t
+tr_bitfieldCountTrueBits( const tr_bitfield * b )
+{
+    assert( b->true_count == tr_bitfieldCountRange( b, 0, b->bit_count ) );
+    return b->true_count;
+}
+
+static inline bool
+tr_bitfieldHasAll( const tr_bitfield * b )
+{
+    return b->bit_count ? ( b->true_count == b->bit_count ) : b->have_all_hint;
+}
+
+static inline bool
+tr_bitfieldHasNone( const tr_bitfield * b )
+{
+    return b->bit_count ? ( b->true_count == 0 ) : b->have_none_hint;
+}
 
 /** A stripped-down version of bitfieldHas to be used
     for speed when you're looping quickly. This version
     has none of tr_bitfieldHas()'s safety checks, so you
     need to call tr_bitfieldTestFast() first before you
     start looping. */
-static inline bool tr_bitfieldHasFast( const tr_bitfield * b, const size_t nth )
+static inline bool
+tr_bitfieldHasFast( const tr_bitfield * b, const size_t n )
 {
-    return ( b->bits[nth>>3u] << ( nth & 7u ) & 0x80 ) != 0;
+    if( tr_bitfieldHasAll( b ) ) return true;
+    if( tr_bitfieldHasNone( b ) ) return false;
+    return ( b->bits[n>>3u] << ( n & 7u ) & 0x80 ) != 0;
 }
 
 /** @param high the highest nth bit you're going to access */
-static inline bool tr_bitfieldTestFast( const tr_bitfield * b, const size_t high )
+static inline bool
+tr_bitfieldTestFast( const tr_bitfield * b, const size_t high )
 {
     return ( b != NULL )
-        && ( b->bits != NULL )
-        && ( high < b->bitCount );
+        && ( high < b->bit_count );
 }
 
-static inline bool tr_bitfieldHas( const tr_bitfield * b, size_t nth )
+static inline bool
+tr_bitfieldHas( const tr_bitfield * b, size_t n )
 {
-    return tr_bitfieldTestFast( b, nth ) && tr_bitfieldHasFast( b, nth );
+    return tr_bitfieldTestFast( b, n )
+        && tr_bitfieldHasFast( b, n );
 }
 
 #endif

commit 05b9c7b3e637d38beb3f9cf104da364832f2f0fc

(trunk libT) fix endgame detection bug introduced in r12248

libtransmission/peer-mgr.c
@@ -874,7 +874,7 @@ testForEndgame( const Torrent * t )
     /* we consider ourselves to be in endgame if the number of bytes
        we've got requested is >= the number of bytes left to download */
     return ( t->requestCount * t->tor->blockSize )
-               <= tr_cpLeftUntilDone( &t->tor->completion );
+               >= tr_cpLeftUntilDone( &t->tor->completion );
 }
 
 static void